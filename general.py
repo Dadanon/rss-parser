250524b5542dc50d242beedbd92b9a1546a920f8
Gubenko_Daniil <gubenko@ia-group.ru>
2024-07-05 16:46:36 +0300

    Initial

---
+++ general.py
@@ -0,0 +1,97 @@
+import re
+from typing import Optional, List
+
+import requests
+
+
+def _get_items(content: str) -> List[re.Match[str]]:
+    return re.findall(patterns['get_items'], content, re.DOTALL)
+
+
+patterns = {
+    'ensure_rss': r'<rss[^>].*?>',
+    'get_channel_info': r'<channel>(.*?)<item>',
+    'get_channel_title': r'',
+    'get_channel_title_and_description': r'<channel>.*?<title>(.*?)</title>.*?<description>(.*?)</description>',
+    'get_items': r'<item>(.*?)</item>',
+    'get_item_title': r'<title>(.*?)</title>',
+    'get_item_link': r'<link>(.*?)</link>',
+    'get_item_description': r'<description>(.*?)</description>',
+    'get_pretty_param': r'<!\[CDATA\[(.*?)]',
+    'get_p_tags': r'<p>(.*?)</p>',
+    'get_opening_tags': r'<[^>].*?>',
+    'get_closing_tags': r'</.*?>',
+    'get_open_close_tags': r'<[^/].*?/>'
+}
+
+
+class Item:
+    title: Optional[str]
+    link: Optional[str]
+    description: Optional[str]
+
+    def __init__(self, title: Optional[str], link: Optional[str], description: Optional[str]):
+        self.title = title
+        self.link = link
+        self.description = description
+        if not self.title and not self.description:
+            raise ValueError('Either title or description should be set')
+
+
+channels = {
+    'RBC': 'https://rssexport.rbc.ru/rbcnews/news/20/full.rss',
+    'Habr': 'https://habr.com/ru/rss/articles/',
+    'NOT_RSS': 'https://qna.habr.com/q/1138620',
+    'Events': 'https://www.opennet.ru/opennews/opennews_review.rss',
+    'BSD': 'https://www.opennet.ru/opennews/opennews_bsd.rss',
+    'Mozilla': 'https://www.opennet.ru/opennews/opennews_mozilla_full.rss',
+    'EurasiaDaily': 'https://eadaily.com/ru/rss/index.xml',
+    'MinisterstvoOborony': 'https://function.mil.ru:443/rss_feeds/reference_to_general.htm?contenttype=xml',
+    'Nant': 'http://www.nantes.fr/rss/content/actualites.rss',
+    'Tartu': 'https://www.tartu.ee/ru/rss'
+}
+
+
+def _get_channel_content(channel_rss_link: str) -> Optional[str]:
+    if channel_rss_link:
+        try:
+            channel_content: str = requests.get(channel_rss_link, timeout=5, verify=False).text
+        except (requests.exceptions.ReadTimeout, requests.exceptions.ProxyError):
+            return None
+        is_rss = re.search(patterns['ensure_rss'], channel_content, re.DOTALL)
+        if is_rss:
+            return channel_content
+
+
+def _get_text_without_tags(text: str) -> str:
+    pretty_text = text
+
+    # Заменить lt gt на закрывающие / открывающие кавычки
+    pretty_text = pretty_text.replace('&lt;', '<').replace('&gt;', '>')
+
+    open_tag_matches = set(re.findall(patterns['get_opening_tags'], pretty_text, re.DOTALL))
+    for open_tag_match in open_tag_matches:
+        pretty_text = pretty_text.replace(open_tag_match, '')
+
+    close_tag_matches = set(re.findall(patterns['get_closing_tags'], pretty_text, re.DOTALL))
+    for close_tag_match in close_tag_matches:
+        pretty_text = pretty_text.replace(close_tag_match, '')
+
+    open_close_tag_matches = set(re.findall(patterns['get_open_close_tags'], pretty_text, re.DOTALL))
+    for open_close_tag_match in open_close_tag_matches:
+        pretty_text = pretty_text.replace(open_close_tag_match, '')
+
+    pretty_text = (
+        pretty_text
+        .replace('\r\n', ' ')
+        .replace('\n', ' ')
+        .replace('&nbsp;', ' ')
+        .replace(u'\xa0', u' ')
+        .replace('&mdash;', '-')
+        .replace(' &laquo;', '"')
+        .replace(' &raquo;', '"')
+    )
+
+    pretty_text = re.sub('\s+', ' ', pretty_text)
+
+    return pretty_text
---
+++ parser.py
@@ -0,0 +1,113 @@
+import re
+import time
+
+from typing import Optional, Dict
+
+from general import _get_channel_content, channels, patterns, Item, _get_text_without_tags
+
+"""Эти данные будут храниться в БД"""
+
+
+class RSSParser:
+    _current_item_index: int
+    _channel_content: Optional[str]
+    _items_dict: Dict[int, Item]
+
+    def __init__(self, channel_rss_link: str):
+        self._items_dict = {}
+        self._channel_content = _get_channel_content(channel_rss_link)
+        if not self._channel_content:
+            raise ValueError(f"Не удалось спарсить RSS контент по указанной ссылке: {channel_rss_link}")
+        self._set_items_dict()
+        self._current_item_index = -1
+
+    # INFO: public methods
+
+    def get_channel_title_and_description(self) -> Optional[Dict[Optional[str], Optional[str]]]:
+        channel_info_match = re.search(patterns['get_channel_info'], self._channel_content, re.DOTALL)
+        if channel_info_match:
+            pretty_title, pretty_description = None, None
+            pretty_title_match = re.search(patterns['get_item_title'], channel_info_match.group(1), re.DOTALL)
+            if pretty_title_match:
+                pretty_title = _get_text_without_tags(pretty_title_match.group(1))
+            pretty_description_match = re.search(patterns['get_item_description'], channel_info_match.group(1), re.DOTALL)
+            if pretty_description_match:
+                pretty_description = _get_text_without_tags(pretty_description_match.group(1))
+            return {'title': pretty_title, 'description': pretty_description}
+
+    def get_next(self) -> Optional[Item]:
+        next_index = self._current_item_index + 1
+        self._current_item_index = next_index if 0 <= next_index < self.items_length else 0
+        return self._prettify_item()
+
+    def get_prev(self) -> Optional[Item]:
+        prev_index = self._current_item_index - 1
+        self._current_item_index = prev_index if 0 <= prev_index < self.items_length else self.items_length - 1
+        return self._prettify_item()
+
+    def get_first(self) -> Optional[Item]:
+        self._current_item_index = 0
+        return self._prettify_item()
+
+    def get_last(self) -> Optional[Item]:
+        self._current_item_index = self.items_length - 1
+        return self._prettify_item()
+
+    @property
+    def items_length(self) -> int:
+        return len(self._items_dict)
+
+    # INFO: private methods
+
+    def _set_items_dict(self):
+        item_matches = re.finditer(patterns['get_items'], self._channel_content, re.DOTALL)
+        start_index = 0
+        for item_match in item_matches:
+            item_str = item_match.group(1)
+
+            item_title_match = re.search(patterns['get_item_title'], item_str, re.DOTALL)
+            item_link_match = re.search(patterns['get_item_link'], item_str, re.DOTALL)
+            item_description_match = re.search(patterns['get_item_description'], item_str, re.DOTALL)
+
+            item_title = item_title_match.group(1) if item_title_match else None
+            item_link = item_link_match.group(1) if item_link_match else None
+            item_description = item_description_match.group(1) if item_description_match else None
+
+            item: Item = Item(item_title, item_link, item_description)
+            self._items_dict[start_index] = item
+            start_index += 1
+
+    def _prettify_item(self) -> Optional[Item]:
+        current_item = self._items_dict.get(self._current_item_index)
+        if current_item:
+            pretty_title_match = re.search(patterns['get_pretty_param'], current_item.title, re.DOTALL)
+            pretty_description_match = re.search(patterns['get_pretty_param'], current_item.description, re.DOTALL)
+
+            pretty_title = pretty_title_match.group(1) if pretty_title_match else current_item.title
+            pretty_description = pretty_description_match.group(1) if pretty_description_match else current_item.description
+            pretty_description = _get_text_without_tags(pretty_description)
+            pretty_title = _get_text_without_tags(pretty_title)
+            pretty_item: Item = Item(pretty_title, current_item.link, pretty_description)
+            return pretty_item
+
+
+def test_rss(channel_rss_link: str):
+    start_time = time.time()
+    parser = RSSParser(channel_rss_link)
+    print(parser.get_channel_title_and_description())
+    print(parser.get_next().__dict__)
+    print(parser.get_next().__dict__)
+    print(parser.get_next().__dict__)
+
+    print(parser.get_prev().__dict__)
+    print(parser.get_prev().__dict__)
+    print(parser.get_prev().__dict__)
+
+    print(parser.get_first().__dict__)
+    print(parser.get_last().__dict__)
+
+    end_time = time.time()
+    print(f'Total time: {end_time - start_time}')
+
+
+test_rss(channels.get('RBC'))
